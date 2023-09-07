a21515bf611c0d66242c5f39c22209be448dcc5a
“yoavkaplan123git config --global user.email “yoav110702@gmail.com <yoav110702@gmail.com>
2023-09-07 17:31:33 +0300

    limlight

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,10 +20,10 @@ public final class Constants {
 
     public static class Camera {
         public static final double CAMERA_DISTANCE_FROM_CENTER_IN_X = 
-            alliance == Alliance.Blue ? 0.415 : 0.415; //TODO
+            alliance == Alliance.Blue ? 0.43 : 0.43; //TODO
             
         public static final double CAMERA_DISTANCE_FROM_CENTER_IN_Y =
-            alliance == Alliance.Blue ? 0.3 : 0.3; //TODO
+            alliance == Alliance.Blue ? 0.414 : 0.414; //TODO
 
         public static final double CAMERA_DISTANCE_FROM_CENTER_IN_Z =
             alliance == Alliance.Blue ? 0.7 : 0.7; //TODO
@@ -51,15 +51,15 @@ public final class Constants {
         public static final double FIELD_WIDTH_METERS = 8.02;
         public static final double FIELD_LENGTH_METERS = 16.54;
         private static final double BLUE_X = 0.143 + Robot.LENGTH / 2d;
-        private static final double RED_X = 14.5;
-        private static final double A1Y = 0.51;
-        private static final double A2Y = 1.06;
-        private static final double A3Y = 1.63;
-        private static final double B1Y = 2.19;
-        private static final double B2Y = 2.74;
-        private static final double B3Y = 3.31;
-        private static final double C1Y = 3.87;
-        private static final double C2Y = 4.42;
+        private static final double RED_X = 14.52;
+        private static final double A1Y = 0.54;
+        private static final double A2Y = 1.09;
+        private static final double A3Y = 1.66;
+        private static final double B1Y = 2.12;
+        private static final double B2Y = 2.77;
+        private static final double B3Y = 3.34;
+        private static final double C1Y = 3.91;
+        private static final double C2Y = 4.45;
         private static final double C3Y = 4.98;
         public static final Translation2d[] ScoringPoses = {
             new Translation2d(
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -124,49 +124,33 @@ public class RobotContainer {
         () -> SwerveDrivetrainSubsystem.getInstance().FactorVelocityTo(1))
     );
 
-    DRIVER_PS4_CONTROLLER.cross().whileTrue(
-      new ShelfIntakeAutomation(0.8
-      )).whileFalse(new SetElvator(0));
+    DRIVER_PS4_CONTROLLER.cross().whileTrue(new AutoAdjustForScore());
 
     DRIVER_PS4_CONTROLLER.povDown().whileTrue(new ResetElevator());
 
-    // OPERATOR_PS4_CONTROLLER.povUp().whileTrue(
-    //     new ShelfIntakeAutomation(IntakeConstance.IntakePowerForCone)
-    //   ).whileFalse(
-    //     new SetElvator(ElevatorConstance.minPose)
-    //   );
+    OPERATOR_PS4_CONTROLLER.povUp().whileTrue(
+        new ShelfIntakeAutomation(IntakeConstance.IntakePowerForCone)
+      ).whileFalse(
+        new SetElvator(ElevatorConstance.minPose)
+      );
 
-    //   OPERATOR_PS4_CONTROLLER.povDown().whileTrue(
-    //     new ShelfIntakeAutomation(IntakeConstance.IntakePowerForCube)
-    //   ).whileFalse(
-    //     new InstantCommand(() -> Elevator.getInstance().setSetPoint(ElevatorConstance.minPose))
-    //   );
+      OPERATOR_PS4_CONTROLLER.povDown().whileTrue(
+        new ShelfIntakeAutomation(IntakeConstance.IntakePowerForCube)
+      ).whileFalse(
+        new InstantCommand(() -> Elevator.getInstance().setSetPoint(ElevatorConstance.minPose))
+      );
 
-    //   OPERATOR_PS4_CONTROLLER.square().whileTrue(
-    //     new SetElvator(Elevator.getInstance().highHight)
-    //   );
+      OPERATOR_PS4_CONTROLLER.square().whileTrue(
+        new SetElvator(Elevator.getInstance().highHight)
+      );
   
-    //   OPERATOR_PS4_CONTROLLER.triangle().whileTrue(
-    //     new SetElvator(ElevatorConstance.lowPose)
-    //   );
+      OPERATOR_PS4_CONTROLLER.triangle().whileTrue(
+        new SetElvator(ElevatorConstance.lowPose)
+      );
   
-    //   OPERATOR_PS4_CONTROLLER.cross().whileTrue(
-    //     new SetElvator(Elevator.getInstance().midhight)
-    //   );
-
-
-    DRIVER_PS4_CONTROLLER.povRight().whileTrue(
-      new SetElvator(Elevator.getInstance().highHight)
-    );
-
-    DRIVER_PS4_CONTROLLER.povLeft().whileTrue(
-      new SetElvator(Elevator.getInstance().midhight)
-      
-    );
-    
-    DRIVER_PS4_CONTROLLER.povUp().whileTrue(
-      new SetElvator(Elevator.getInstance().lowHight)
-    );
+      OPERATOR_PS4_CONTROLLER.cross().whileTrue(
+        new SetElvator(Elevator.getInstance().midhight)
+      );
   }
 
   /**
